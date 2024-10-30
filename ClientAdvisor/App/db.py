87b58457ea994d9067d840b24b8d76283e645cdb
gpickett <122489228+gpickett@users.noreply.github.com>
2024-10-30 14:02:35 -0700

    updated container name

--- ClientAdvisor/App/db.py
+++ ClientAdvisor/App/db.py
@@ -1,23 +1,35 @@
 # db.py
 import os
-import pymssql
+# import pymssql
+import psycopg2
 from dotenv import load_dotenv
 
 load_dotenv()
 
-server = os.environ.get('SQLDB_SERVER')
-database = os.environ.get('SQLDB_DATABASE')
-username = os.environ.get('SQLDB_USERNAME')
-password = os.environ.get('SQLDB_PASSWORD')
+# server = os.environ.get('SQLDB_SERVER')
+# database = os.environ.get('SQLDB_DATABASE')
+# username = os.environ.get('SQLDB_USERNAME')
+# password = os.environ.get('SQLDB_PASSWORD')
+
+server = os.environ.get('POSTGRESQL_SERVER')
+database = os.environ.get('POSTGRESQL_DATABASENAME')
+username = os.environ.get('POSTGRESQL_USER')
+password = os.environ.get('POSTGRESQL_PASSWORD')
+sslmode='require'
+
 
 def get_connection():
 
-    conn = pymssql.connect(
-        server=server,
-        user=username,
-        password=password,
-        database=database,
-        as_dict=True
-    )  
+    # conn = pymssql.connect(
+    #     server=server,
+    #     user=username,
+    #     password=password,
+    #     database=database,
+    #     as_dict=True
+    # )  
+    # return conn
+
+    # Construct connection URI
+    db_uri = f"postgresql://{username}:{password}@{server}/{database}?sslmode={sslmode}"
+    conn = psycopg2.connect(db_uri) 
     return conn
- 
--- ClientAdvisor/Deployment/scripts/create_azure_functions.sh
+++ ClientAdvisor/Deployment/scripts/create_azure_functions.sh
@@ -37,7 +37,7 @@ az storage account create --name $storageAccount --location eastus --resource-gr
 az functionapp create --resource-group $resourceGroupName --name $functionappname \
                 --environment $env_name --storage-account $storageAccount \
                 --functions-version 4 --runtime python \
-                --image bycwacontainerregps.azurecr.io/byc-wa-fn-ps1:latest
+                --image bycwacontainerregps.azurecr.io/byc-wa-fn-ps:latest
 
 # Sleep for 120 seconds
 echo "Waiting for 120 seconds to ensure the Function App is properly created..."
